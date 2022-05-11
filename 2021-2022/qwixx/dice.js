adee7721ff1f9889d4ddc4ad5f87d7c5e0d15da2
Adam Bourg <Adam.Bourg@pwc.com>
2022-05-11 13:58:20 -0600

    wip

--- 2021-2022/qwixx/dice.js
+++ 2021-2022/qwixx/dice.js
@@ -1,60 +1,59 @@
 // https://codepen.io/Pyremell/pen/eZGGXX/ 
 
 var dice = {
-    sides: 6,
-    roll: function () {
-      var randomNumber = Math.floor(Math.random() * this.sides) + 1;
-      return randomNumber;
-    }
+  sides: 6,
+  roll: function () {
+    var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+    return randomNumber;
+  }
+}
+
+
+
+//Prints dice roll to the page
+
+function printNumber(number, dieNumber) {
+  var placeholder = document.getElementById(dieNumber);
+  placeholder.innerHTML = number;
+}
+
+var button = document.getElementById('diceRoll');
+
+button.onclick = function() {
+  var [
+    white1, 
+    white2, 
+    blue, 
+    yellow,
+    green,
+    red
+  ] = [
+    dice.roll(),
+    dice.roll(),
+    dice.roll(),
+    dice.roll(),
+    dice.roll(),
+    dice.roll()
+  ];
+
+  // taking the color values and storing them into a global scope
+  window.colorValues = {
+    white1, 
+    white2, 
+    blue, 
+    yellow,
+    green,
+    red
   }
   
-  
-  
-  //Prints dice roll to the page
-  
-  function printNumber(number, dieNumber) {
-    var placeholder = document.getElementById(dieNumber);
-    placeholder.innerHTML = number;
-  }
-  
-  var button = document.getElementById('diceRoll');
-  
-  button.onclick = function() {
-    var [
-      white1, 
-      white2, 
-      blue, 
-      yellow,
-      green,
-      red
-    ] = [
-      dice.roll(),
-      dice.roll(),
-      dice.roll(),
-      dice.roll(),
-      dice.roll(),
-      dice.roll()
-    ];
+  printNumber(white1, 'die1');
+  printNumber(white2, 'die2');
+  printNumber(blue, 'die3');
+  printNumber(yellow, 'die4');
+  printNumber(green, 'die5');
+  printNumber(red, 'die6');
 
-    // taking the color values and storing them into a global scope
-    window.colorValues = {
-      white1, 
-      white2, 
-      blue, 
-      yellow,
-      green,
-      red
-    }
-    
-    printNumber(white1, 'die1');
-    printNumber(white2, 'die2');
-    printNumber(blue, 'die3');
-    printNumber(yellow, 'die4');
-    printNumber(green, 'die5');
-    printNumber(red, 'die6');
+  var event = new Event('diceRolled');
+  document.dispatchEvent(event); 
+};
 
-    var event = new Event('diceRolled');
-    document.dispatchEvent(event); 
-  };
-  
-  
